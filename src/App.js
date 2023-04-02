60557e5b0b1fb2f9fb57c65e13ddcafe25f12fbb
Manuquim <manuelcabreraosorio11@gmail.com>
2023-04-02 21:25:28 +0200

    020423

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { UseFetch } from './useFetch';
 
 function App() {
+  
+  const url='https://jsonplaceholder.typicode.com/users';
+  const { data , loading , error}=UseFetch(url);
+  
+  const handleOpen=(x)=>{
+   alert("Estamos: "+x);
+    //return <input type="text" value={x}/>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fetch like a PRO</h1>
+      
+      <div className='card'>
+        {error && <h2>Error:{error}</h2>}
+        {loading && <h2>loading...</h2>}
+        <ul className='mapeo'>
+          {data?.map((user)=><li key={user.id}><button onClick={()=>handleOpen(user.name)}>{user.name} </button></li>)}  
+          {/*data?.map((user)=><li key={user.id}>{user.name}</li>)*/}     
+        </ul> 
+      </div>
     </div>
   );
 }
